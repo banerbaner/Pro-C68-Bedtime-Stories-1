0e59b697d144d077342c57a0c4f665d52444eb0f
Rehan Siddiqui <rehanbaner@gmail.com>
2020-05-12 21:05:50 +0530

    added files for project 69

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
@@ -13,18 +13,41 @@ export default class App extends React.Component {
     );
   }
 }
- 
-const TabNavigator = createBottomTabNavigator({
-  Write: { screen: WriteScreen },
-  Read: { screen: ReadScreen },
-});
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Read: { screen: ReadScreen },
+    Write: { screen: WriteScreen },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ }) => {
+        const routeName = navigation.state.routeName
+        if (routeName === 'Write') {
+          return (
+            <Image
+              source={require('./assets/writer_logo.png')}
+              style={{ width: 40, height: 40 }} />
+          )
+        }
+        else if (routeName === 'Read') {
+          return (
+            <Image
+              source={require('./assets/reader_logo.png')}
+              style={{ width: 40, height: 40 }} />
+          )
+        }
+      }
+    })
+  }
+
+);
 const AppContainer = createAppContainer(TabNavigator);
-/*
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#808080',
     justifyContent: 'center',
   },
-});*/
+});
--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class WriteScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>WriteScreen</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.inputBox}
+            placeholder='Write Here'
+            multiline
+            editable
+          />
+        </View>
       </View>
     );
   }
@@ -16,5 +24,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  }, inputView: {
+    flexDirection: 'row',
+    margin: 20,
+  },
+  inputBox: {
+    width: 300,
+    height: 500,
+    borderWidth: 1.5,
+    borderRightWidth: 0,
+    fontSize: 20
   },
 });
